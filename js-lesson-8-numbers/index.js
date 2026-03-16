949a3d1c738cece650204d0c5f2f19adaff33936
iamteamstar <azadkocak29@gmail.com>
2026-03-16 14:52:33 +0300

    feat(numbers) number methods practice

---
+++ js-lesson-8-numbers/index.js
@@ -0,0 +1,18 @@
+let result=25
+result=Number("25")
+result=parseInt("243")
+result=parseFloat("12")
+console.log(typeof(result))
+result=isNaN("wew23")
+console.log(result)
+let myNumber=12.23462
+result=myNumber.toPrecision(5)//toplam kaç karakter gelecek
+console.log(result)
+myNumber=12.7213354
+result=myNumber.toFixed(4)//noktadan sonra kaç basamak olacak
+console.log(result)
+result=Math.ceil(1.2)//bir üst tam sayıya yuvarla
+result=Math.floor(1.7)//bir alt tam sayıya yuvarla
+result=Math.pow(4,3)//4 üzeri üç hesaplar
+result=Math.sqrt(100)//karekök alır
+console.log(result)
