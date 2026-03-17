55205f8b4bb756c7ad52ca6c94e40ff1e4e9fc86
iamteamstar <azadkocak29@gmail.com>
2026-03-17 16:24:35 +0300

    review :stutent avarega method updated

--- mini-projects/not-ort-hesaplama/index.js
+++ mini-projects/not-ort-hesaplama/index.js
@@ -1,32 +1,28 @@
 function ortHesapla(){
-
-let dersInput1=document.getElementById("lesson1").value
-let dersInput2=document.getElementById("lesson2").value
-let dersInput3=document.getElementById("lesson3").value
-let dersInput4=document.getElementById("lesson4").value
-let dersInput5=document.getElementById("lesson5").value
-
-if(dersInput1===""||dersInput1<0||dersInput1>100 ||dersInput2==""||dersInput2<0||dersInput2>100 ||dersInput3===""||
-    dersInput3<0||dersInput3>100 ||dersInput4===""||dersInput4<0||dersInput4>100 ||dersInput5===""||dersInput5<0||dersInput5>100 )
+let toplam=0;
+let ortalama=0
+let text=document.querySelector("#btnCalculator")
+for(let i=1;i<=5;i++)
 {
-document.querySelector("#btnCalculator").innerHTML="lütfen kutuları doldur veya alanlar geçerli ifadelerle doldur";
+let dersInput=document.getElementById(`lesson${i}`).value
+
+if(dersInput===""||dersInput<0||dersInput>100||isNaN(dersInput) )
+{
+text.innerHTML="lütfen kutuları doldur veya alanlar geçerli ifadelerle doldur";
 return;
 }
 
-let ders1=Number(dersInput1);
-let ders2=Number(dersInput2);
-let ders3=Number(dersInput3);
-let ders4=Number(dersInput4);
-let ders5=Number(dersInput5);
+toplam+=Number(dersInput);
 
-let ortalama=(ders1+ders2+ders3+ders4+ders5)/5;
+}
+ortalama=toplam/5;
 
 if(ortalama<50)
 {
-document.querySelector("#btnCalculator").innerHTML="ortalamanız: "+ortalama.toFixed(2) +"durumunuz: kaldı"
+text.innerHTML="ortalamanız: "+ortalama.toFixed(2) +"durumunuz: kaldı"
 }
 else if(ortalama>=50)
 {
-document.querySelector("#btnCalculator").innerHTML="ortalamanız: "+ortalama.toFixed(2) +"durumunuz: geçti"
+text.innerHTML="ortalamanız: "+ortalama.toFixed(2) +"durumunuz: geçti"
 }
 }
